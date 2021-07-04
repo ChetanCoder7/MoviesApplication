0f5855c6bd24fe0689d78f6f7e0171c071af16fe
ChetanPalkar <chetan.palkr@gmail.com>
2021-07-04 23:32:17 +0530

    inserting records for db table

---
+++ moviesApp/src/main/resources/data.sql
@@ -0,0 +1,113 @@
+
+INSERT INTO STILLS values(1,'11','https://m.media-amazon.com/images/M/MV5BNTYxOTYyMzE3NV5BMl5BanBnXkFtZTcwOTMxNDY3Mw@@._V1_UY99_CR24,0,99,99_AL_.jpg');
+INSERT INTO STILLS values(2,'11','https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL__QL50.jpg');
+INSERT INTO STILLS values(3,'11','https://m.media-amazon.com/images/M/MV5BNzAwOTk3MDg5MV5BMl5BanBnXkFtZTcwNjQxNDY3Mw@@._V1_UY99_CR29,0,99,99_AL_.jpg');
+
+INSERT INTO SOUNDEFFECTS VALUES (1,'21','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (2,'21','SDDS');
+
+INSERT INTO MOVIES values(1,'The Shawshank Redemption','ENGLISH','DELHI','Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.','https://m.mediaamazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL50_.jpg','21','11','tt0111161','NOW_SHOWING','9.2');
+
+
+INSERT INTO STILLS values(4,'12','https://i.imgur.com/3fJ1P48.gif');
+INSERT INTO STILLS values(5,'12','https://i.imgur.com/j6ECXmD.gif');
+INSERT INTO STILLS values(6,'12','https://i.imgur.com/v0ooIH0.gif');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (3,'22','DOLBY');
+INSERT INTO SOUNDEFFECTS VALUES (4,'22','DTS');
+
+INSERT INTO MOVIES values(2,'Harry Potter and the Chamber of Secrets','ENGLISH','PUNE','Forced to spend his summer holidays with his muggle relations, Harry Potter gets a real shock when he gets a surprise visitor: Dobby the house-elf, who warns Harry Potter against returning to Hogwarts, for terrible things are going to happen. Harry decides to ignore Dobby''s warning and continues with his pre-arranged schedule. But at Hogwarts, strange and terrible things are indeed happening: Harry is suddenly hearing mysterious voices from inside the walls, muggle-born students are being attacked, and a message scrawled on the wall in blood puts everyone on his/her guard - ''The Chamber Of Secrets Has Been Opened. Enemies Of The Heir, Beware''.','https://images-na.ssl-images-amazon.com/images/M/MV5BMTcxODgwMDkxNV5BMl5BanBnXkFtZTYwMDk2MDg3._V1_SX300.jpg','22','12','tt0295297','NOW_SHOWING','7.4');
+
+INSERT INTO STILLS values(7,'13','https://i.imgur.com/i3aD05u.png');
+INSERT INTO STILLS values(8,'13','https://i.imgur.com/ABinULO.gif');
+INSERT INTO STILLS values(9,'13','https://i.imgur.com/Wflfyct.gif');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (5,'23','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (6,'23','SDDS');
+
+INSERT INTO MOVIES values(3,'Harry Potter and the Deathly Hallows: Part 2','HINDI','DELHI','Harry, Ron, and Hermione continue their quest of finding and destroying the Dark Lord''s three remaining Horcruxes, the magical items responsible for his immortality. But as the mystical Deathly Hallows are uncovered, and Voldemort finds out about their mission, the biggest battle begins and life as they know it will never be the same again.','https://images-na.ssl-images-amazon.com/images/M/MV5BMjIyZGU4YzUtNDkzYi00ZDRhLTljYzctYTMxMDQ4M2E0Y2YxXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg','23','13','tt1201607','NOW_SHOWING','8.1');
+
+
+INSERT INTO STILLS values(10,'14','https://m.media-amazon.com/images/M/MV5BYTgzZTJlMDUtMGIxNy00ODJiLWI3NjAtYzQ4OTQ3MGQ3ZGYwXkEyXkFqcGdeQXVyMDc2NTEzMw@@._V1_UX99_CR0,0,99,99_AL_.jpg');
+INSERT INTO STILLS values(11,'14','https://m.media-amazon.com/images/M/MV5BMTczMTk5MjkwOF5BMl5BanBnXkFtZTgwMDI0Mjk1NDM@._V1_UY99_CR12,0,99,99_AL_.jpg');
+INSERT INTO STILLS values(12,'14','https://m.media-amazon.com/images/M/MV5BMTI2ODEzMTI1MF5BMl5BanBnXkFtZTYwNjI3MDU2._V1_UX100_CR0,0,100,100_AL_.jpg');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (7,'24','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (8,'24','SDDS');
+
+INSERT INTO MOVIES values(4,'The Godfather','HINDI','BANGALORE','The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.','https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL50_SY1000_CR0,0,704,1000_AL_.jpg','24','14','tt0068646','UP_COMING','8.1');
+
+
+
+INSERT INTO STILLS values(13,'15','https://m.media-amazon.com/images/M/MV5BMTM5NjA1OTYyOV5BMl5BanBnXkFtZTcwMzgzMTk2Mw@@._V1_UY99_CR69,0,99,99_AL_.jpg');
+INSERT INTO STILLS values(14,'15','https://m.media-amazon.com/images/M/MV5BOTk5NDczOTg3N15BMl5BanBnXkFtZTcwMTgzMTk2Mw@@._V1_UY99_CR67,0,99,99_AL_.jpg');
+INSERT INTO STILLS values(15,'15','https://m.media-amazon.com/images/M/MV5BMTkyMjQ4ODk1NF5BMl5BanBnXkFtZTcwMjcxMTk2Mw@@._V1_UX100_CR0,0,100,100_AL_.jpg');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (9,'25','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (10,'25','SDDS');
+
+INSERT INTO MOVIES values(5,'The Dark Knight','HINDI','BANGALORE','Harry, Ron, and Hermione continue their quest of finding and destroying the Dark Lord''s three remaining Horcruxes, the magical items responsible for his immortality. But as the mystical Deathly Hallows are uncovered, and Voldemort finds out about their mission, the biggest battle begins and life as they know it will never be the same again.','https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_QL50_SY1000_CR0,0,675,1000_AL_.jpg','25','15','tt1201607','NOW_SHOWING','8.1');
+
+
+INSERT INTO STILLS values(16,'16','https://i.imgur.com/i3aD05u.png');
+INSERT INTO STILLS values(17,'16','https://i.imgur.com/ABinULO.gif');
+INSERT INTO STILLS values(18,'16','https://i.imgur.com/Wflfyct.gif');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (11,'26','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (12,'26','SDDS');
+
+INSERT INTO MOVIES values(6,'Harry Potter and the Sorcerer''s Stone','HINDI','CHENNAI','This is the tale of Harry Potter, an ordinary 11-year-old boy serving as a sort of slave for his aunt and uncle who learns that he is actually a wizard and has been invited to attend the Hogwarts School for Witchcraft and Wizardry. Harry is snatched away from his mundane existence by Hagrid, the grounds keeper for Hogwarts, and quickly thrown into a world completely foreign to both him and the viewer. Famous for an incident that happened at his birth, Harry makes friends easily at his new school. He soon finds, however, that the wizarding world is far more dangerous for him than he would have imagined, and he quickly learns that not all wizards are ones to be trusted.','https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_QL50_SY1000_CR0,0,675,1000_AL_.jpg','26','16','tt0241527','NOW_SHOWING','7.6');
+
+
+INSERT INTO STILLS values(19,'17','https://i.imgur.com/i3aD05u.png');
+INSERT INTO STILLS values(20,'17','https://i.imgur.com/ABinULO.gif');
+INSERT INTO STILLS values(21,'17','https://i.imgur.com/Wflfyct.gif');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (13,'27','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (14,'27','SDDS');
+
+INSERT INTO MOVIES values(7,'Harry Potter and the Order of the Phoenix','ENGLISH','BANGALORE','After a lonely summer on Privet Drive, Harry returns to a Hogwarts full of ill-fortune. Few of students and parents believe him or Dumbledore that Voldemort is really back. The ministry had decided to step in by appointing a new Defence Against the Dark Arts teacher that proves to be the nastiest person Harry has ever encountered. Harry also can''t help stealing glances with the beautiful Cho Chang. To top it off are dreams that Harry can''t explain, and a mystery behind something Voldemort is searching for. With these many things Harry begins one of his toughest years at Hogwarts School of Witchcraft and Wizardry.','https://images-na.ssl-images-amazon.com/images/M/MV5BMTM0NTczMTUzOV5BMl5BanBnXkFtZTYwMzIxNTg3._V1_SX300.jpg','27','17','tt0373889','UP_COMING','7.5');
+
+
+INSERT INTO STILLS values(22,'18','https://i.imgur.com/i3aD05u.png');
+INSERT INTO STILLS values(23,'18','https://i.imgur.com/ABinULO.gif');
+INSERT INTO STILLS values(24,'18','https://i.imgur.com/Wflfyct.gif');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (15,'28','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (16,'28','SDDS');
+
+INSERT INTO MOVIES values(8,'Harry Potter and the Prisoner of Azkaban','ENGLISH','BANGALORE','Harry Potter is having a tough time with his relatives (yet again). He runs away after using magic to inflate Uncle Vernon''s sister Marge who was being offensive towards Harry''s parents. Initially scared for using magic outside the school, he is pleasantly surprised that he won''t be penalized after all. However, he soon learns that a dangerous criminal and Voldemort''s trusted aide Sirius Black has escaped from the Azkaban prison and wants to kill Harry to avenge the Dark Lord. To worsen the conditions for Harry, vile creatures called Dementors are appointed to guard the school gates and inexplicably happen to have the most horrible effect on him. Little does Harry know that by the end of this year, many holes in his past (whatever he knows of it) will be filled up and he will have a clearer vision of what the future has in store...','https://images-na.ssl-images-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg','28','18','tt0304141','NOW_SHOWING','7.8');
+
+
+INSERT INTO STILLS values(25,'19','https://i.imgur.com/i3aD05u.png');
+INSERT INTO STILLS values(26,'19','https://i.imgur.com/ABinULO.gif');
+INSERT INTO STILLS values(27,'19','https://i.imgur.com/Wflfyct.gif');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (17,'29','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (18,'29','SDDS');
+
+INSERT INTO MOVIES values(9,'Harry Potter and the Goblet of Fire','HINDI','PUNE','Harry''s fourth year at Hogwarts is about to start and he is enjoying the summer vacation with his friends. They get the tickets to The Quidditch World Cup Final but after the match is over, people dressed like Lord Voldemort''s ''Death Eaters'' set a fire to all the visitors'' tents, coupled with the appearance of Voldemort''s symbol, the ''Dark Mark'' in the sky, which causes a frenzy across the magical community. That same year, Hogwarts is hosting ''The Triwizard Tournament'', a magical tournament between three well-known schools of magic : Hogwarts, Beauxbatons and Durmstrang. The contestants have to be above the age of 17, and are chosen by a magical object called Goblet of Fire. On the night of selection, however, the Goblet spews out four names instead of the usual three, with Harry unwittingly being selected as the Fourth Champion. Since the magic cannot be reversed, Harry is forced to go with it and brave three exceedingly difficult tasks.','https://images-na.ssl-images-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_SX300.jpg','29','19','tt0330373','NOW_SHOWING','7.7');
+
+
+INSERT INTO STILLS values(28,'20','https://i.imgur.com/i3aD05u.png');
+INSERT INTO STILLS values(29,'20','https://i.imgur.com/ABinULO.gif');
+INSERT INTO STILLS values(30,'20','https://i.imgur.com/Wflfyct.gif');
+
+
+INSERT INTO SOUNDEFFECTS VALUES (19,'30','RX6');
+INSERT INTO SOUNDEFFECTS VALUES (20,'30','SDDS');
+
+INSERT INTO MOVIES values(10,'Harry Potter and the Deathly Hallows: Part 1','ENGLISH','PUNE','Voldemort''s power is growing stronger. He now has control over the Ministry of Magic and Hogwarts. Harry, Ron, and Hermione decide to finish Dumbledore''s work and find the rest of the Horcruxes to defeat the Dark Lord. But little hope remains for the Trio, and the rest of the Wizarding World, so everything they do must go as planned.','https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2OTE1Mjk0N15BMl5BanBnXkFtZTcwODE3MDAwNA@@._V1_SX300.jpg','30','20','tt0926084','NOW_SHOWING','7.7');
+
+
+
+
+
