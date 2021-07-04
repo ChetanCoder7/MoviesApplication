e283c9afabf4843d73a3b54b8d7526fd093eda39
ChetanPalkar <chetan.palkr@gmail.com>
2021-07-04 23:30:50 +0530

    Default Creation Of Project With H2 Database

---
+++ moviesApp/src/test/java/com/hollywood/moviesApp/MoviesAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hollywood.moviesApp;
+
+import org.junit.jupiter.api.Test;
+
+public class MoviesAppApplicationTests {
+
+	@Test
+	public void contextLoads() throws Exception {
+
+	}
+
+}
